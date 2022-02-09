7568dcc2ad132214085cf8e795fc2478846c5bbb
Junior <juniorgarciadiaz@outlook.com>
2022-02-09 16:30:05 +0000

    Efecto de scroll y creación de global.scss

--- src/components/Topbar/topbar.jsx
+++ src/components/Topbar/topbar.jsx
@@ -2,8 +2,18 @@ import ('./topbar.scss');
 
 export default function Topbar() {
     return (
-        <div className='topbar'>
-            
+        <div className='topbar' id='topbar'>
+            <div clasname='wrapper'>
+                <div className='left'>
+                    <a href='#intro'>
+                        intro
+                    </a>
+                </div>
+
+                <div className='right'>
+
+                </div>
+            </div>
         </div>
     )
 }
