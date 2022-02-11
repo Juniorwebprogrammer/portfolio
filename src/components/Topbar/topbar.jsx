3b737ec6d493850951276b74a93223bfd769ad95
Junior <juniorgarciadiaz@outlook.com>
2022-02-11 15:13:36 +0000

    Cración de iconos y barras de menú

--- src/components/Topbar/topbar.jsx
+++ src/components/Topbar/topbar.jsx
@@ -1,4 +1,6 @@
+import {Person, Mail} from '@material-ui/icons';
 import ('./topbar.scss');
+
 
 export default function Topbar() {
     return (
@@ -9,13 +11,21 @@ export default function Topbar() {
                         JS
                     </a>
                     <div className='itemcontainer'>
-
+                        <Person className='icon'/>
+                        <span>+34 697 928 123</span>
                     </div>
-                    <h1>
-                        Menu
-                    </h1>
+                    <div className='itemcontainer'>
+                        <Mail className='icon'/>
+                        <span> juniorgarciadiaz@outlook.com </span>
+                    </div>
+                    <div className='menu'>
+                        <span className='line-1'></span>
+                        <span className='line-2'></span>
+                        <span className='line-3'></span>
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
+
