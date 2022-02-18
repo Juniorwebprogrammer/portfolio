192611ccab183c09052dd5f6f0b1a8263b1f5557
Junior <juniorgarciadiaz@outlook.com>
2022-02-18 20:45:40 +0000

    Terminado la Topbar e inicio del componente intro

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -3,7 +3,18 @@ import ('./intro.scss');
 export default function Intro() {
     return (
         <div className='intro' id='intro'>
-
+            <div className='left'>
+                <div className="imgContainer">
+                    <img src="" alt="" />
+                </div>
+            </div>
+            <div className='right'>
+                <div className="wrapper">
+                    <h2>Hi there I'm</h2>
+                    <h1>Junior García</h1>
+                    <h3>Developer<span></span></h3>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Menu/menu.jsx
+++ src/components/Menu/menu.jsx
@@ -3,19 +3,19 @@ import ("./menu.scss")
 export default function ListMenu({menuOpen, setMenuOpen}) {
     return (
         <div className={"listMenu " + (menuOpen && "active")}>
-            <li>
+            <li onClick={()=> setMenuOpen(false)} >
                 <a href="#intro"> Home </a>
             </li>
-            <li>
+            <li onClick={()=> setMenuOpen(false)} >
                 <a href="#portfolio"> Portfolio </a>
             </li>
-            <li>
+            <li onClick={()=> setMenuOpen(false)} >
                 <a href="#works"> Works </a>
             </li>
-            <li>
+            <li onClick={()=> setMenuOpen(false)} >
                 <a href="#testimonials"> Testimonials </a>
             </li>
-            <li>
+            <li onClick={()=> setMenuOpen(false)} >
                 <a href="#contact"> Contact </a>
             </li>
         </div>
