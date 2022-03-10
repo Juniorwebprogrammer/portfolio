7b01f340b91f95c68c9ace545bde1310783d768d
Junior <juniorgarciadiaz@outlook.com>
2022-03-10 19:25:10 +0000

    Incorporación de logos en componente intro

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -1,18 +1,41 @@
+import css from './assets/css.png';
+import html from './assets/html.png';
+import js from './assets/JS.png';
+import react from './assets/react.png';
+import node from './assets/node.png';
 import ('./intro.scss');
+
 
 export default function Intro() {
     return (
         <div className='intro' id='intro'>
             <div className='left'>
-                <div className="imgContainer">
-                    <img src="" alt="" />
+                <div className='titleContainer'>
+                    <h2> I have worked with the following technologies </h2>
+                </div>
+                <div className='imageContainer'>
+                    <div className='img'>
+                        <img src={html} alt='html'/>
+                        <img src={css} alt='css'/>
+                        <img src={js} alt='js'/>
+                    </div>
+                </div>
+                <div className='imageContainer_2'>
+                    <div className='img_2'>
+                        <img src={react} alt='react'/>
+                    </div>
+                </div>
+                <div className='imageContainer_3'>
+                    <div className='img_3'>
+                        <img src={node} alt='node'/>
+                    </div>
                 </div>
             </div>
             <div className='right'>
                 <div className="wrapper">
                     <h2>Hi there I'm</h2>
                     <h1>Junior García</h1>
-                    <h3>Developer<span></span></h3>
+                    <h3>Full stack Developer<span></span></h3>
                 </div>
             </div>
         </div>
