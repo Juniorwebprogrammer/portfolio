8318acb84ce84bb30018f139882b3412539bfee6
Junior <juniorgarciadiaz@outlook.com>
2022-03-14 19:35:35 +0000

    Creación básica de formulario y envio de email

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -1,9 +1,31 @@
+import emailjs from '@emailjs/browser';
 import ('./contact.scss');
 
 export default function Contact() {
+    const sendemail = (e) =>{
+        e.preventDefault();
+
+        emailjs.sendForm('service_8au44bn', 'template_f0ykppb', e.target, '5kJpBHtdASkT5SWIc')
+        .then(res=>console.log(res))
+        .catch(error=>console.log(error))
+    };
+
     return (
         <div className='contact' id='contact'>
-
+            <form className='form' onSubmit={sendemail}>
+               
+                <label>Name</label>
+                <input type='text' className='form-control' id='name' name='nombre'/>
+                
+                
+                <label>E-mail</label>
+                <input type='email' className='form-control' id='email' name='correo'/> 
+               
+                <label>Message</label>
+                <input type='text' className='form-control' id='message'name='mensaje'/>
+                
+                <input type="submit" value="Send e-mail"></input>
+            </form>
         </div>
     )
 }
